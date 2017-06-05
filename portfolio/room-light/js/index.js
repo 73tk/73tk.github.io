d135869e539b25950e5b5fdf4ddbc594d01d0778
73tk <namihey0203@gmail.com>
2017-06-05 13:39:12 +0900

    portfolioをcodepenに差し替え

--- portfolio/room-light/js/index.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  'use strict';
-  $('.switch').on('click', function () {
-    $('.room').toggleClass('on');
-    $(this).find('i').first().appendTo(this);
-    return false;
-  });
-});
